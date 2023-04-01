49e3d175d5b44d42d920dc6f6ad35cba96fc7b0f
Ankit Kumar Singh <tiknaks@gmail.com>
2023-04-01 10:41:57 +0530

    Final Project

---
+++ src/components/Pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./about.css";
+
+export const About = () => {
+  return (
+    <div className="about-container">
+      <h1 className="about-heading">About</h1>
+      <p className="about-text">
+        Hello and welcome to my video resume! My name is <strong>Ankit Kumar Singh</strong>, and I am excited to share with you my skills, experience, and achievements throughout my bachelor's in computer science and engineering having a domain in web development.
+      </p>
+      <p className="about-text">
+        I have a good grip in coding languages like C, C++, and JavaScript and basic knowledge of Python and Java as well. I have a good understanding of AngularJS and Django, and I am also a quick learner. In addition to my ongoing degree, I have completed a few web development courses, and the various technologies I have learned have helped me develop my knowledge.
+      </p>
+      <p className="about-text">
+        I have also been involved in extracurricular activities such as being an active NCC cadet, where I got to learn and improve my leadership qualities, teamwork, unity, and discipline. My strength is logic building with good analytical skills, and I really enjoy solving problems. I am always keen to learn new software that would help me improve my skills for further growth. I am a deep learner and have good time management skills and have never faced any problem working in a team.
+      </p>
+      <p className="about-text">
+        This internship would provide me an excellent opportunity to learn and grow and contribute the best for the company. Thank you for taking the time to watch my video. I hope I've been able to convey my passion for your prestigious company, as well as my experience and strengths. If you think I'd be a good fit for your company, I would love to hear from you. You can find more information about me and my work on my website, or connect with me on LinkedIn at mandeepkala. Thanks again for considering my application.
+      </p>
+    </div>
+  );
+};
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div style={{ backgroundColor: "transparent", padding: "20px" }}>
+      <h1 style={{ color: "#333", fontSize: "32px", marginBottom: "20px" }}>Home</h1>
+      <p style={{ color: "#CE5959", fontWeight:"bolder",fontSize: "18px", lineHeight: "1.5" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+    </div>
+  );
+};
